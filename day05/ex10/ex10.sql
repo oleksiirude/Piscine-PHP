a27c6f0f502c73cbabe0d98c1bc4b96c0f4ef519
Oleksii RUDENKO <olrudenk@e2r11p10.unit.ua>
2019-04-09 22:05:58 +0300

    day05 done

---
+++ day05/ex10/ex10.sql
@@ -0,0 +1,3 @@
+SELECT `title` AS 'Title', `summary` AS `Summary`, `prod_year` FROM `film`, `genre`
+	WHERE genre.id_genre = film.id_genre AND genre.name = 'erotic'
+	ORDER BY `prod_year` DESC;
---
+++ day05/ex15/ex15.sql
@@ -0,0 +1,2 @@
+SELECT REVERSE(SUBSTRING(`phone_number`, 2)) AS 'rebmunenohp' FROM `distrib`
+	WHERE `phone_number` REGEXP "^05";
